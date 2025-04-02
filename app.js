b28eb735e0e4976736a22c8a71f0bdbd507df626
Ana Cris <anacris.muniz03@outlook.com>
2025-04-02 19:49:38 -0300

    deixando o jogo facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listasDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 30;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do Numero Secreto");
-  exibirTextoNaTela("p", "Escolha um numero entre 1 e 50");
+  exibirTextoNaTela("p", "Escolha um numero entre 1 e 30");
 }
 
 exibirMensagemInicial();
